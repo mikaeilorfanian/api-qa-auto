f78c8a3fd5007563cfb2e2912f411b9a4f7b96c0
Mikaeil Orfanian <mokt@outlook.com>
2020-01-05 15:16:29 +0100

    Code format fix

--- test_api/test_add_post.py
+++ test_api/test_add_post.py
@@ -8,6 +8,7 @@ class TestAddPost:
         assert post_creation_resp.status_code == 201
 
         resp_body = post_creation_resp.json()
+
         verify_json_payload_for_post_object(resp_body)
 
         id_of_created_post = resp_body['id']
@@ -16,6 +17,7 @@ class TestAddPost:
         assert get_post_resp.status_code == 200
 
         resp_body = get_post_resp.json()
+
         verify_json_payload_for_post_object(resp_body)
 
     def test_adding_post_with_GET_http_verb_fails(self, http_client, post_object):
